ee490188ec9dd028753e3d17d0282b768b500592
CARLOS EDUARDO Santos Almeida <cadu.almeida.cc99@outlook.com>
2022-01-03 22:16:38 -0300

    finalizando despesas e criando views/controller do salario

---
+++ Models/Despesas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.Models
+{
+    public class Despesas
+    {
+        public int DespesaId { get; set; }
+
+        public int MesId { get; set; }
+        public Meses Meses { get; set; }
+
+        public int TipoDespesaId { get; set; }
+        public TipoDespesas TipoDespesas { get; set; }
+
+        [Required(ErrorMessage = "Compo Obrigatório.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Valor Inválido")]
+        public double Valor { get; set; }
+
+    }
+}
---
+++ Models/TipoDespesas.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.Models
+{
+    public class TipoDespesas
+    {
+        public int TipoDespesasID { get; set; }
+
+        [Required(ErrorMessage = "Compo Obrigatório.")]
+        [StringLength(50, ErrorMessage = "Use menos caracteres.")]
+        [Remote("TipoDespesaExiste", "TipoDespesas")]
+        public string Nome { get; set; }
+
+        public ICollection<Despesas> Despesas { get; set; }
+    }
+}
